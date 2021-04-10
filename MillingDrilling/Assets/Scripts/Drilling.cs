a49d08ac39b47203d72d55422f3269241f99e38f
Maelle <selsynn@live.fr>
2021-04-10 08:01:08 -0700

    Unity setup + fix test in unity test runner framework

---
+++ MillingDrilling/Assets/Scripts/Drilling.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Scripts
+{
+
+    //- assuming x,y plan
+    //- assuming starting boring is in {0,d/2}. This is the top part of the circle.
+    //- angleShift moves start boring to the right (now top part but on the right.)
+
+    [System.Serializable]
+    public class Drillings
+    {
+
+        [System.Serializable]
+        public class Drilling
+        {
+            //X in mm
+            public double X;
+            //X in mm
+            public double Y;
+            //Angle in degree
+            public double Angle;
+        }
+
+        public List<Drilling> ListDrillings { get; }
+
+        public Drillings(double diameter, uint nbDrillings, double angleShift)
+        {
+            ListDrillings = new List<Drilling>((int)nbDrillings);
+            double angle = (angleShift+90) % 360;
+            for (uint i = 0; i < nbDrillings; i++)
+            {
+                double x = (double)diameter / 2 * Math.Cos(angle * Math.PI / 180);
+                double y = (double)diameter / 2 * Math.Sin(angle * Math.PI / 180);
+                Drilling b = new Drilling();
+                b.Angle = angle;
+                b.X = x;
+                b.Y = y;
+                ListDrillings.Add(b);
+
+                angle -= (360 / (double)nbDrillings) % 360;
+            }
+        }
+    }
+} 
---
+++ MillingDrilling/Assets/Tests/DrillingTest.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using Scripts;
+
+namespace Tests
+{
+    public class DrillingTest
+    {
+        [Test]
+        public void TestWhen1()
+        {
+            List<Drillings.Drilling> borings = new Drillings(10, 1, 0).ListDrillings;
+            Assert.AreEqual(borings.Count, 1);
+            Drillings.Drilling boring = borings[0];
+            Assert.AreEqual(boring.Angle, 0);
+            Assert.AreEqual(boring.X, 0);
+            Assert.AreEqual(boring.Y, 10);
+        }
+
+        [Test]
+        public void TestWhen2Tilted()
+        {
+            List<Drillings.Drilling> borings = new Drillings(10, 2, 90).ListDrillings;
+            Assert.AreEqual(borings.Count, 2);
+            Drillings.Drilling boring = borings[0];
+            Assert.AreEqual(boring.Angle, 90);
+            Assert.AreEqual(boring.X, 10);
+            Assert.AreEqual(boring.Y, 0);
+
+            boring = borings[1];
+            Assert.AreEqual(boring.Angle, 270);
+            Assert.AreEqual(boring.X, -10);
+            Assert.AreEqual(boring.Y, 0);
+        }
+    }
+}
