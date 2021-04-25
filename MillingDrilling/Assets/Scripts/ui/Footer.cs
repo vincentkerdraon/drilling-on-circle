083b11ae137eff01c22c13f516a51bc2e20fdb8a
Maelle <selsynn@live.fr>
2021-04-24 17:02:11 -0700

    update footer to reflect what the current tab is

--- MillingDrilling/Assets/Scripts/ui/Footer.cs
+++ MillingDrilling/Assets/Scripts/ui/Footer.cs
@@ -16,6 +16,9 @@ namespace ui {
         [SerializeField]
         private GameObject pParams, pVisu, pAide;
 
+        [SerializeField]
+        private Sprite iActive, iInactive;
+
 
         // Start is called before the first frame update
         void Start()
@@ -25,18 +28,27 @@ namespace ui {
                 pParams.SetActive(true);
                 pVisu.SetActive(false);
                 pAide.SetActive(false);
+                bParams.image.sprite = iActive;
+                bVisu.image.sprite = iInactive;
+                bHelp.image.sprite = iInactive;
             });
             bVisu.onClick.AddListener(delegate {
                 Debug.Log("button visu click");
                 pParams.SetActive(false);
                 pVisu.SetActive(true);
                 pAide.SetActive(false);
+                bParams.image.sprite = iInactive;
+                bVisu.image.sprite = iActive;
+                bHelp.image.sprite = iInactive;
             }); 
             bHelp.onClick.AddListener(delegate {
                 Debug.Log("button help click");
                 pParams.SetActive(false);
                 pVisu.SetActive(false);
                 pAide.SetActive(true);
+                bParams.image.sprite = iInactive;
+                bVisu.image.sprite = iInactive;
+                bHelp.image.sprite = iActive;
             });
         }
     }
